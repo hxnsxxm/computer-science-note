7d921ed03b88a2915de2fff0165df9caa9f036db
hxnsxxm <108333175+hxnsxxm@users.noreply.github.com>
2023-03-06 17:56:11 +0900

    싱글톤 패턴 개념 연습

---
+++ src/designpattern/Singleton01.java
@@ -0,0 +1,22 @@
+package designpattern;
+
+class Singleton {
+	private static class singleInstanceHolder {
+		private static final Singleton INSTANCE = new Singleton();
+	}
+	public static synchronized Singleton getInstance() {
+		return singleInstanceHolder.INSTANCE;
+	}
+}
+
+public class Singleton01 {
+	public static void main(String[] args) {
+		Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        if (a == b) {
+            System.out.println(true);
+        }
+	}
+}
