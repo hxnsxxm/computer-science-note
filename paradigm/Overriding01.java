4c9a31cd5893ead69461d45c3b834618ec8cd543
hxnsxxm <108333175+hxnsxxm@users.noreply.github.com>
2023-03-07 22:55:41 +0900

    오버라이딩 기본 개념

---
+++ paradigm/Overriding01.java
@@ -0,0 +1,24 @@
+
+
+class Animal {
+	public void bark() {
+		System.out.println("mumu! mumu!");
+	}
+}
+
+class Dog extends Animal {
+	@Override
+	public void bark() {
+		System.out.println("wal!! wal!!");
+	}
+}
+
+public class Overriding01 {
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.bark();
+	}
+}
+/*
+wal!! wal!!
+*/
